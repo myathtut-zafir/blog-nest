3c45fa341ec11b0bd48fbc6b5c901bd5fa16ceec
myathtut-zafir <aa.htut@gmail.com>
2025-08-31 14:19:20 +0700

    feat: add package

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
+    ConfigModule.forRoot({ isGlobal: true }),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION') || '1h',
+        },
+      }),
+    }),
+  ],
+})
 export class IamModule {}
